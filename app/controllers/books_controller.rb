cfd619151dd18eaceeda1f91b72a0cb1f8e4834f
EC2 Default User <ec2-user@ip-172-31-22-225.ec2.internal>
2022-05-04 08:16:50 +0000

    バリデーションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice1: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -24,9 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice2: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
